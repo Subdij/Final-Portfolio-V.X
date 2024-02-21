6261941607567107d3f3bb30a6ca78199f53624f
Subdij <ziyadou2011@hotmail.fr>
2024-02-21 18:00:10 +0100

    update

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+
+function filterProjects(category) {
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        const itemCategories = item.classList;
+
+        if (category === 'all' || itemCategories.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
